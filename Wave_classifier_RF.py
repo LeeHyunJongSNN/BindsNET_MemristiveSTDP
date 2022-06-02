20561c4fbd58848bd14a6732601efdd0077410f2
이현종 <74046436+LeeHyunJongSNN@users.noreply.github.com>
2022-06-02 17:29:46 +0900

    Normalization, Removing DC offset and FFT

--- Wave_classifier_RF.py
+++ Wave_classifier_RF.py
@@ -44,10 +44,10 @@ parser.add_argument("--n_train", type=int, default=1)
 parser.add_argument("--n_workers", type=int, default=-1)
 parser.add_argument("--exc", type=float, default=90)
 parser.add_argument("--inh", type=float, default=480)
-parser.add_argument("--theta_plus", type=float, default=0.0005)
+parser.add_argument("--theta_plus", type=float, default=0.003)
 parser.add_argument("--time", type=int, default=500)
 parser.add_argument("--dt", type=int, default=1)
-parser.add_argument("--intensity", type=float, default=600)
+parser.add_argument("--intensity", type=float, default=200)
 parser.add_argument("--encoder", dest="encoder_type", default="PoissonEncoder")
 parser.add_argument("--progress_interval", type=int, default=10)
 parser.add_argument("--update_interval", type=int, default=1)
@@ -158,7 +158,7 @@ classes = []
 
 fname = " "
 for fname in ["C:/Pycharm BindsNET/Wave_classifier/Simple_Waves_RF/"
-              "(sine+sawtooth)_1kHz_40_amplitude_0dB_20000.txt"]:
+              "(sine+sawtooth)_1kHz_10_amplitude_0dB_20000.txt"]:
     print(fname)
     f = open(fname, "r", encoding='utf-8-sig')
     n_attack = 0
@@ -173,19 +173,11 @@ for fname in ["C:/Pycharm BindsNET/Wave_classifier/Simple_Waves_RF/"
         if len(linedata) == 0:
             continue
 
-        linedata_dcremoved = [x for x in linedata[0:len(linedata) - 1]]
-        linedata_dcremoved -= np.mean(linedata_dcremoved)
-        linedata_dcremoved = detrend(linedata_dcremoved)    # removing DC offset
-
-        linedata_fft1 = np.fft.fft([x for x in linedata_dcremoved[0:10]]) / 10
-        linedata_fft2 = np.fft.fft([x for x in linedata_dcremoved[10:20]]) / 10
-        linedata_fft3 = np.fft.fft([x for x in linedata_dcremoved[20:30]]) / 10
-        linedata_fft4 = np.fft.fft([x for x in linedata_dcremoved[30:40]]) / 10
-
-        linedata_fft = linedata_fft1.tolist() + linedata_fft2.tolist() + linedata_fft3.tolist() + linedata_fft4.tolist()
+        linedata_labelremoved = minmax_scale([x for x in linedata[0:len(linedata) - 1]]).tolist()
+        linedata_dcremoved = linedata_labelremoved - np.mean(linedata_labelremoved)   # removing DC offset
+        linedata_fft = np.fft.fft(linedata_dcremoved).tolist()
         linedata_abs = [abs(x) for x in linedata_fft[0:len(linedata_fft)]]
-        linedata_normalized = np.round(minmax_scale(linedata_abs), 5).tolist()
-        linedata_intensity = [intensity * x for x in linedata_normalized[0:len(linedata_abs)]]
+        linedata_intensity = [intensity * round(x, 6) for x in linedata_abs[0:len(linedata_abs)]]
 
         cl = int(linedata[-1])
         classes.append(cl)
