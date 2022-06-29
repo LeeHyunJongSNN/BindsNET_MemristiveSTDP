3a49660074b0a9b30919661f0fb7a04f2bbf86cc
이현종 <74046436+LeeHyunJongSNN@users.noreply.github.com>
2022-06-29 23:55:16 +0900

    Delete bindsnet directory

--- bindsnet/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .models import (
-    TwoLayerNetwork,
-    DiehlAndCook2015,
-    DiehlAndCook2015_NonLinear,
-    DiehlAndCook2015v2,
-    IncreasingInhibitionNetwork,
-    LocallyConnectedNetwork,
-)
